fdf41e633394e8b500d1a1e17fd04085fe35c0a9
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2024-06-27 17:00:40 +0100

    Add MQTT server connection UI and logic
    - MainForm.Designer.cs: Removed `components` initialization and added UI elements (`lblServerAddress`, `txtServerAddress`, `btnConnect`, `lblStatus`) for MQTT server connection.
    - MainForm.cs: Added `using` directives for threading and MQTTnet. Introduced `mqttClient` field and `InitializeMqttClient` method. Implemented `btnConnect_Click` for server connection.
    - MainForm.resx: Added new resource file with XML schema, resource headers.
    - PoC MQTT Messaging App.csproj: Included `MainForm.resx` as an `EmbeddedResource`.

--- PoC MQTT Messaging App/MainForm.Designer.cs
+++ PoC MQTT Messaging App/MainForm.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblServerAddress = new System.Windows.Forms.Label();
+            this.txtServerAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblServerAddress
+            // 
+            this.lblServerAddress.AutoSize = true;
+            this.lblServerAddress.Location = new System.Drawing.Point(12, 9);
+            this.lblServerAddress.Name = "lblServerAddress";
+            this.lblServerAddress.Size = new System.Drawing.Size(72, 13);
+            this.lblServerAddress.TabIndex = 0;
+            this.lblServerAddress.Text = "MQTT Server";
+            // 
+            // txtServerAddress
+            // 
+            this.txtServerAddress.Location = new System.Drawing.Point(90, 6);
+            this.txtServerAddress.Name = "txtServerAddress";
+            this.txtServerAddress.Size = new System.Drawing.Size(160, 20);
+            this.txtServerAddress.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(256, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(717, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(71, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "disconnected";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtServerAddress);
+            this.Controls.Add(this.lblServerAddress);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblServerAddress;
+        private System.Windows.Forms.TextBox txtServerAddress;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- PoC MQTT Messaging App/MainForm.cs
+++ PoC MQTT Messaging App/MainForm.cs
@@ -5,16 +5,58 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MQTTnet;
+using MQTTnet.Client;
 
 namespace PoC_MQTT_Messaging_App
 {
     public partial class MainForm : Form
     {
+        private IMqttClient mqttClient;
         public MainForm()
         {
             InitializeComponent();
+            InitializeMqttClient();
+        }
+
+        // Initialise MQTT client
+        private void InitializeMqttClient()
+        {
+            var factory = new MqttFactory();
+            mqttClient = factory.CreateMqttClient();
+
+            // Setup MQTT event handlers
+            mqttClient.ConnectedAsync += e =>
+            {
+                Invoke(new Action(() =>
+                {
+                    lblStatus.Text = "Connected";
+                    lblStatus.ForeColor = Color.Green;
+                }));
+                return Task.CompletedTask;
+            };
+            mqttClient.DisconnectedAsync += e =>
+            {
+                Invoke(new Action(() =>
+                {
+                    lblStatus.Text = "Disconnected";
+                    lblStatus.ForeColor = Color.Red;
+                }));
+                return Task.CompletedTask;
+            };
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithClientId("PoC_MQTT_Messaging_App")
+                .WithTcpServer(txtServerAddress.Text)
+                .Build();
+
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
         }
     }
 }
